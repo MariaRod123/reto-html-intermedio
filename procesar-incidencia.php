12d03eacb554c39485110d156c27efa1dafa000f
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-02 18:04:07 -0300

    Add files via upload

---
+++ procesar-incidencia.php
@@ -0,0 +1,24 @@
+
+
+<?php
+echo "<h2> Datos recibidos </h2>";
+
+$usuario = $_POST['usuario'];
+$departamento = $_POST['departamento'];
+$descripcion= $_POST['descripcion'];
+$contrasenia = $_POST['contrasenia'];
+
+echo "<br/> &nbsp; Usuario: " . $usuario;
+echo "<br/> &nbsp; Departamento: " . $departamento;
+echo "<br/> &nbsp; Incidencia: " . $descripcion;
+echo "<br/> &nbsp; Contraseña: " . $contrasenia;
+
+
+?>
+
+
+
+
+
+
+
